723b085fb4924f9404b871b36480c5cae8fdaf44
Nicolas Ziegler <nicolas.ziegler@sbs-herzogenaurach.de>
2025-03-31 01:18:22 +0200

    finish

--- plot.py
+++ plot.py
@@ -6,7 +6,7 @@ import pandas as pd
 df=pd.read_csv("iris.csv")
 
 
-plt.scatter(df["sepal_length"],df["sepal_width"],c="blue",alpha=0.5)
+plt.scatter(df["sepal.length"],df["sepal.width"],c="blue",alpha=0.5)
 
 plt.xlabel("SepalLength")
 plt.ylabel("SepalWidth")
@@ -18,7 +18,7 @@ plt.show()
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-sns.scatterplot(data=df,x="sepal_length",y="sepal_width",hue="species")
+sns.scatterplot(data=df,x="sepal.length",y="sepal.width",hue="species")
 
 plt.xlabel("SepalLength")
 plt.ylabel("SepalWidth")
